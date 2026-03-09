6e9e830d36eb4174adfc189415de06579bc336b5
Bob-Maximus <1elkabbanyou@hdsb.ca>
2026-03-09 16:51:59 -0400

    Added a simple character controller with a player one and 2
    player one has arrow keys, player 2 has wasd

---
+++ Bad Game Ideas Game/Assets/Player/Scripts/Groundcheck.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Groundcheck : MonoBehaviour
+{
+    public bool grounded;
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ Bad Game Ideas Game/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] public bool player1;
+
+    public Groundcheck groundcheck;
+
+    public float speed;
+    public float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1)
+        {
+            Control1();
+        } else
+        {
+            Control2();
+        }
+    }
+
+    public void Control1()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(-speed);
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            Move(speed);
+        } else
+        {
+            Move(0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump(jumpHeight);
+        }
+    }
+
+    public void Control2()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(-speed);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(speed);
+        } else
+        {
+            Move(0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump(jumpHeight);
+        }
+    }
+
+    public void Move(float speed)
+    {
+        rb.velocityX = Mathf.Lerp(rb.velocityX, speed, Time.deltaTime*15);
+    }
+
+    public void Jump(float jumpHeight)
+    {
+        if (groundcheck.grounded)
+        {
+            rb.velocityY = jumpHeight;
+        }
+    }
+}
